8d88f8eb12677a7a25c3f82f7cb27d4467915968
Horrid Entity <etienne.olieu@gmail.com>
2023-03-03 10:03:31 +0100

    Added barracks and guards, js and html

--- 03._Plain_HTML_Server/app.js
+++ 03._Plain_HTML_Server/app.js
@@ -2,23 +2,12 @@ const express = require("express")
 const app = express()
 app.use(express.static("public"))
 
-const tanks = [
-    {name: "Leopard", nationality: "Germany", version: "1"},
-    {name: "Centurion", nationality: "United Kingdom", year: 1946},
-    {name: "Sho't", nationality: "Israel", year: 1960},
-    {name: "Strv", nationality: "Sweden", year: 1967},
-    {name: "Olifant", nationality: "South Africa", year: 1974},
-    {name: "Bernardini MB-3 Tamoyo", nationality: "Brazil", year: 1983},
-    {name: "Zulifaqir I", nationality: "Iran", year: 1994},
-    {name: "Ramses II", nationality: "Egypt", year: 2005},
-    {name: "AMX-40", nationality: "France", year: 1983},
-    {name: "Pokpung-ho II", nationality: "North Korea", year: 1992},
-    {name: "Landkreauzer P 1500 Monster", nationality: "Germany", year: 1942},
-    {name: "T-42", nationality: "Russia", year: 1932},
-    {name: "M1 Abrams", version: "M1"}
-]
+const  { getTanks, addTank } = require("./util/tanks.js");
+//const {getTanks, addTank} = tanksUtil.getTanks();
 
 let visitorCount = 0;
+
+/* Pages */
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/frontpage/frontpage.html")
@@ -32,11 +21,19 @@ app.get("/tanks", (req, res) => {
     res.sendFile(__dirname + "/public/tanks/tanks.html")
 });
 
+app.get("/museumGuards", (req, res) => {
+    res.sendFile(__dirname + "/public/museumGuards/museumGuards.html");
+})
+
 //___________API'er HER______________
 
 app.get("/api/tanks", (req, res) => {
-    res.send({ data: tanks })
+    res.send({ data: tanks });
 });
+
+app.get("/api/guards", (req,res) => {
+    res.send({ data: guards});
+})
 
 app.get("/api/visitors", (req, res) => {
     res.send({ data: visitorCount });
---
+++ 03._Plain_HTML_Server/public/museumGuards/barracks.js
@@ -0,0 +1,16 @@
+const guards = [
+    "Hank the tank",
+    "Thomas the tank engine",
+    "CEPOS"
+] 
+
+/*
+function getGuards(){
+    return guards;
+}
+*/
+
+function addGuard(guard) {
+    guards.push(guard);
+    return guards;
+}
