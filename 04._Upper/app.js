9c0d74cf29dc210db53a518181de72bc22b7a316
Horrid Entity <etienne.olieu@gmail.com>
2023-03-24 10:20:06 +0100

    made forms and a contact page

--- 04._Upper/app.js
+++ 04._Upper/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 const app = express();
 app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}))
 import templateEngine from "./util/templateEngine.js";
 
 const frontpage =  templateEngine.readPage("./public/pages/frontpage/frontpage.html");
@@ -17,6 +18,11 @@ const quotes = templateEngine.readPage("./public/pages/quotes/quotes.html");
 const quotesPage = templateEngine.renderPage(quotes, {
     tabTitle: "UPPER | Quotes"
 });
+
+const contact = templateEngine.readPage("./public/pages/contact/contact.html");
+const contactPage = templateEngine.renderPage(contact, {
+    tabTitle: "UPPER | Contact"
+})
 
 //path er en import der ligger native i node. Skal bruges nu hvor hele systemet er et modul. VIGTIGT er IKKE at have en skråstreg foran stiens navn.
 app.get("/", (req, res) => {
@@ -36,6 +42,10 @@ app.get("/quotes", (req ,res) => {
     res.send(quotesPage);
 })
 
+app.get("/contact", (req, res) => {
+    res.send(contactPage);
+})
+
 /* --- API --- */
 
 app.get("/api/jokes", (req, res) => {
@@ -43,7 +53,14 @@ app.get("/api/jokes", (req, res) => {
     console.log(jokes.getJoke())
 })
 
-const PORT = 8080
+app.post("/api/contact", (req, res) => {
+    res.redirect("/");
+    console.log(req.body)
+})
+
+console.log(process.env.ENV)
+
+const PORT = Number(process.env.PORT) || 8082; //Henter port fra env eller giver en default port hvis ingen bliver defineret, af den ene eller anden årsag.
 app.listen(PORT, (error) => {
     if (error){
         console.log(error);
--- 04._Upper/public/pages/quests/quests.js
+++ 04._Upper/public/pages/quests/quests.js
@@ -1,8 +1,19 @@
-const sourceUrl = "https://www.boredapi.com/api/activity/"
 
-fetch(sourceUrl)
-.then(response => response.json())
-.then(result => {
-    document.getElementById("quest").innerText = result.activity;
-});
+function fetchQuest(activityQueryString=""){
+    console.log(activityQueryString)
+    fetch("https://www.boredapi.com/api/activity/"+activityQueryString
+    )
+    .then(response => response.json())
+    .then(result => {
+        document.getElementById("quest").innerText = result.activity;
+    });
+}
+//fetchQuest(); //udkommenteret for ikke at belaste api
 
+function getNewQuest() {
+    const dropdown = document.getElementById("activity-dropdown")
+    fetchQuest(`?type=${dropdown.value}`)
+    console.log(dropdown.value);
+}
+
+document.getElementById("fetch-new-quest-btn").addEventListener("click", getNewQuest) //function reference means function gets executed when button is clicked
